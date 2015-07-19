55751b88afc6ed1c0b999a51d6d84af815acce21
Ashish Goel <ashishg656@gmail.com>
2015-07-19 23:41:18 +0530

    persist images

--- wsgi/bookncart/bookncart/settings.py
+++ wsgi/bookncart/bookncart/settings.py
@@ -128,5 +128,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(os.environ.get('OPENSHIFT_DATA_DIR', ''),'media')
 MEDIA_URL = '/media/'
