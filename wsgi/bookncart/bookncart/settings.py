d7b01cba77ccaf70ab2c4d37f98249639e279fd9
Ashish Goel <ashishg656@gmail.com>
2015-07-19 12:21:59 +0530

    changed database to mysql from sqlite

--- wsgi/bookncart/bookncart/settings.py
+++ wsgi/bookncart/bookncart/settings.py
@@ -57,11 +57,22 @@ WSGI_APPLICATION = 'bookncart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         # GETTING-STARTED: change 'db.sqlite3' to your sqlite3 database:
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        # GETTING-STARTED: change 'db.sqlite3' to your sqlite3 database:
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'adminHBWYWin',
+        'PASSWORD': 'CwtIMnkHxhhv',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
