1e5dbc0bdd66b2ae96bffaee2b7bd1d8ba170895
Ashish Goel <ashishg656@gmail.com>
2015-07-19 20:16:35 +0530

    s

--- wsgi/bookncart/bookncart/urls.py
+++ wsgi/bookncart/bookncart/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     url('', include('bookncart_web.urls', namespace='bookncart_web')),
     url(r'^admin/', include(admin.site.urls)),
 ]
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
